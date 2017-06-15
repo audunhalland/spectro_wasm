d4a3fb0c6a8d686d37fe9f51b087dd1d90d39463
Audun Halland <audun.halland@knowit.no>
2017-06-16 01:05:43 +0200

    Analyze samples in rust! It works!

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
+set -e
+
 cargo build --target=wasm32-unknown-emscripten --verbose
 
 cp target/wasm32-unknown-emscripten/debug/deps/* www/
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #![feature(link_args)]
 
-#[link_args = "-s EXPORTED_FUNCTIONS=['_test']"]
+#[link_args = "-s EXPORTED_FUNCTIONS=['_analyze']"]
 extern {}
 
+use std::slice;
+
 #[no_mangle]
-pub fn test() {
-    println!("rust: test!");
+pub unsafe fn analyze(buf: *const f32, length: usize) {
+    analyze_internal(slice::from_raw_parts(buf, length));
+}
+
+fn analyze_internal(buf: &[f32]) {
+    println!("{} of {} samples has energy",
+             buf.iter().fold(0, |count, sample| if *sample == 0.0 { count } else { count + 1 } ),
+             buf.len());
 }
 
 // When exporting test, main is not exported at all.
