7bfff3e8f8aaf0b430bf210cf1c35f6d13cc79c2
Audun Halland <audun.halland@knowit.no>
2017-06-16 15:40:30 +0200

    Perform the actual FFT.

--- serve.sh
+++ serve.sh
@@ -2,9 +2,11 @@ set -e
 
 cargo build --target=wasm32-unknown-emscripten --verbose
 
-cp target/wasm32-unknown-emscripten/debug/deps/* www/
+cp target/wasm32-unknown-emscripten/debug/deps/*.js www/
+cp target/wasm32-unknown-emscripten/debug/deps/*.wasm www/
+cp target/wasm32-unknown-emscripten/debug/deps/*.wast www/
 
 # Currently *.js.map triggers weird exceptions in browser.
-rm www/*.js.map
+# rm www/*.js.map
 
 http-server www
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ extern {}
 
 use std::slice;
 
+extern crate rustfft;
+
+use rustfft::num_complex::Complex;
+
 #[no_mangle]
 pub unsafe fn analyze(buf: *const f32, length: usize) {
     analyze_internal(slice::from_raw_parts(buf, length));
@@ -14,6 +18,16 @@ fn analyze_internal(buf: &[f32]) {
     println!("{} of {} samples has energy",
              buf.iter().fold(0, |count, sample| if *sample == 0.0 { count } else { count + 1 } ),
              buf.len());
+
+    let mut input = Vec::new();
+    for sample in buf {
+        input.push(Complex { re: *sample, im: 0.0 });
+    }
+    let mut output = vec![Complex { re: 0.0, im: 0.0 }; buf.len()];
+
+    let mut planner = rustfft::FFTplanner::new(false);
+    let fft = planner.plan_fft(buf.len());
+    fft.process(&mut input, &mut output);
 }
 
 // When exporting test, main is not exported at all.
