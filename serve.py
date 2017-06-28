ac13842218af22f32809b81c22c4554f7577ab96
Audun Halland <audun.halland@gmail.com>
2017-06-29 00:00:32 +0200

    Shorten max line length in serve.py

--- serve.py
+++ serve.py
@@ -4,8 +4,10 @@ import glob, os, subprocess, shutil
 
 subprocess.check_call(['cargo', 'build', '--target=wasm32-unknown-emscripten'])
 
+dbg_pattern = 'target/wasm32-unknown-emscripten/debug/deps/*-????????????????.%s'
+
 for ext in ['js', 'wasm', 'wast']:
-    shutil.copy(sorted(glob.iglob('target/wasm32-unknown-emscripten/debug/deps/*-????????????????.%s' % ext),
+    shutil.copy(sorted(glob.iglob(dbg_pattern % ext),
                        key=os.path.getctime)[-1],
                 'www/spectro2.%s' % ext)
 
