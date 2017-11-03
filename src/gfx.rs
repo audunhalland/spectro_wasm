bdd65bd912dbe8306b28c938ebe5a77c1d6de371
Audun Halland <audun.halland@gmail.com>
2017-11-03 15:46:41 +0100

    Flip byte order!

--- src/gfx.rs
+++ src/gfx.rs
@@ -10,10 +10,10 @@ pub struct Color {
 
 impl Color {
     pub fn as_u32(&self) -> u32 {
-        (self.r as u32) << 24
-            | (self.g as u32) << 16
-            | (self.b as u32) << 8
-            | (self.a as u32)
+        (self.r as u32) << 0
+            | (self.g as u32) << 8
+            | (self.b as u32) << 16
+            | (self.a as u32) << 24
     }
 }
 
